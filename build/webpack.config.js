01bf316d002c5d010902f17ec54df1874903c5bd
Alex Nudelman <nudelx@gmail.com>
2016-08-12 01:20:53 +0300

    clean

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -103,18 +103,18 @@ use a linting plugin for your IDE in place of this loader.
 If you do wish to continue using the loader, you can uncomment
 the code below and run `npm i --save-dev eslint-loader`. This code
 will be removed in a future release.
-
-webpackConfig.module.preLoaders = [{
-  test: /\.(js|jsx)$/,
-  loader: 'eslint',
-  exclude: /node_modules/
-}]
-
-webpackConfig.eslint = {
-  configFile: paths.base('.eslintrc'),
-  emitWarning: __DEV__
-}
 */
+// webpackConfig.module.preLoaders = [{
+//   test: /\.(js|jsx)$/,
+//   loader: 'eslint',
+//   exclude: /node_modules/
+// }]
+//
+// webpackConfig.eslint = {
+//   configFile: paths.base('.eslintrc'),
+//   emitWarning: __DEV__
+// }
+
 
 // ------------------------------------
 // Loaders
