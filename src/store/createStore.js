c0b6194f0dc95d6da8942734a5f3a7768ea2586e
Alex Nudelman <nudelx@gmail.com>
2016-08-12 00:48:15 +0300

    arch rebuilded

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,42 +1,28 @@
 import { applyMiddleware, compose, createStore } from 'redux'
 import { routerMiddleware } from 'react-router-redux'
-import thunk from 'redux-thunk'
-import makeRootReducer from './reducers'
-
+import reducers from './reducers'
+// console.log('red', reducers)
 export default (initialState = {}, history) => {
-  // ======================================================
-  // Middleware Configuration
-  // ======================================================
-  const middleware = [thunk, routerMiddleware(history)]
 
-  // ======================================================
-  // Store Enhancers
-  // ======================================================
-  const enhancers = []
-  if (__DEBUG__) {
-    const devToolsExtension = window.devToolsExtension
-    if (typeof devToolsExtension === 'function') {
-      enhancers.push(devToolsExtension())
-    }
-  }
+  const enhancer = compose(
+    // Middleware you want to use in development:
+    applyMiddleware(
+      routerMiddleware(history)
+    ),
+    // Required! Enable Redux DevTools with the monitors you chose
+    window.devToolsExtension ? window.devToolsExtension() : f => f
+  );
 
   // ======================================================
   // Store Instantiation and HMR Setup
   // ======================================================
-  const store = createStore(
-    makeRootReducer(),
-    initialState,
-    compose(
-      applyMiddleware(...middleware),
-      ...enhancers
-    )
-  )
-  store.asyncReducers = {}
+  // const middlewares = []
+  const store = createStore(reducers, initialState, enhancer);
 
   if (module.hot) {
-    module.hot.accept('./reducers', () => {
-      const reducers = require('./reducers').default
-      store.replaceReducer(reducers(store.asyncReducers))
+    module.hot.accept('store/reducers', () => {
+      const nextRootReducer = reducers.default
+      store.replaceReducer(nextRootReducer)
     })
   }
 
