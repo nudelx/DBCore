cb029e057e5b6d34df3dadd8977e5ca91ca9e0d1
Alex Nudelman <nudelx@gmail.com>
2016-08-13 15:35:08 +0300

    first cmp

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,10 +1,23 @@
 import React, { PropTypes } from 'react';
+import AppBar from 'material-ui/AppBar';
+import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
-const AppContainer = ({ location }) => (
-  <div>{`current pah is ${location.pathname}`}</div>
+const AppContainer = () => (
+  <div>
+    <MuiThemeProvider muiTheme={getMuiTheme(baseTheme)}>
+      <AppBar
+        title='Jopa Book'
+        iconClassNameRight='muidocs-icon-navigation-expand-more'
+      />
+    </MuiThemeProvider>
+  </div>
 );
+
 
 AppContainer.propTypes = {
   location: PropTypes.object
 };
+
 export default AppContainer;
