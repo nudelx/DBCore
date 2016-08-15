064d21e6c74e72c294f6335382f37014642da58e
nudelx <alex.nudelman@samanage.com>
2016-08-15 18:23:52 +0300

    demo

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -4,13 +4,15 @@ import baseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import injectTapEventPlugin from 'react-tap-event-plugin';
+import classNames from 'classnames';
+import classes from './test.scss';
 
 injectTapEventPlugin();
-
 const AppContainer = () => (
   <div>
     <MuiThemeProvider muiTheme={getMuiTheme(baseTheme)}>
       <AppBar
+        className={classNames('ddd', 'dddddd', classes.fuck)}
         title='Jopa Book'
         iconClassNameRight='muidocs-icon-navigation-expand-more'
       />
